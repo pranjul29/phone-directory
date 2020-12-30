45d50f3e8b0b690e2927aae92c4a5a1a5f54e7b7
pranjul29 <paras.paharia29@gmail.com>
2020-12-30 19:07:17 +0530

    Added ‘PhoneDirectory’ as smart component, renamed ‘App’ as ‘ShowSubscribers’ and converted ‘AddSubscribers’ and ‘ShowSubscribers’ as dumb components

--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -1,14 +1,8 @@
 import React, {Component} from 'react';
 import Header from "./Header"
-import "./App.css"
+import "./ShowSubscribers.css"
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            subscribersListToShow: []
-        }
-    }
+class ShowSubscribers extends Component {
 
     render() {
 
@@ -23,7 +17,7 @@ class App extends Component {
                         <span className="grid-item phone-heading">Phone</span>
                     </div>
                     {
-                        this.state.subscribersListToShow.map(sub => {
+                        this.props.subscribersList.map(sub => {
                             return <div key={sub.id} className="grid-container">
                                 <span className="grid-item">{sub.name}</span>
                                 <span className="grid-item">{sub.phone}</span>
@@ -37,4 +31,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default ShowSubscribers;
